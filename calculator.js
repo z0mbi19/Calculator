43e77c8a6c6e351c5356239670adb142a1b42a19
z0mbi19 <32523746+z0mbi19@users.noreply.github.com>
2020-04-08 00:53:46 -0300

    express and bodyParser

--- calculator.js
+++ calculator.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res){
-  res.send("Helo World")
+  
+  res.sendFile(__dirname + "/index.html");
+
 });
+
+app.post("/", function(req, res){
+
+  let num1 = req.body.num1
+  let num2 = req.body.num2
+
+  let soma = Number(num1) + Number(num2)
+
+  res.send("O resultado é " + soma)
+  
+})
 
 app.listen(3000, function(){
   console.log("Server rodando porta 3000");
